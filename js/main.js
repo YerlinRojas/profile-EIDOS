777545dc22d6a9a24fe31d1bd4e14f66855c5e86
Yerlin ROJAS <yerlinrojas808@gmail.com>
2023-05-03 12:50:11 -0300

    archivo js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log('¡Hola mundo!');
+
+let num1 = 50;
+let num2 = 10;
+let saludo = '¡Hola soy yer!';
+let nombreCurso = 'Código LATAM COMI 11';
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoMultiplicacion = num1 * num2;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + 'Esto es ' + nombreCurso);
+
