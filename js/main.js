219c8fd400cf697eec1aa19fab8984ec0d799c28
Yerlin ROJAS <yerlinrojas808@gmail.com>
2023-07-14 12:12:35 -0300

    profile

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 document.getElementById('boton-descargar').addEventListener('click', function() {
-    // URL del documento que deseas descargar
     var url = './YerlinRojas200-.pdf';
 
-    // Crea un enlace temporal
     var link = document.createElement('a');
     link.href = url;
 
-    // Establece el nombre del archivo
     link.download = './YerlinRojas200-.pdf';
 
-    // Simula un clic en el enlace para descargar el archivo
     link.click();
 })
